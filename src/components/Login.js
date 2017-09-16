e6fd9d2bd4dc749252f67ff5acaa04ff36f13c4a
demiladebamgbose <demiladebamgbose94@gmail.com>
2017-09-16 21:32:43 +0100

    set up ui flow

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Login extends Component {
 
-  render () {
-    return (
 
+  render () {
+
+    const loginBtnStyle = {
+      margin:'auto',
+      width:'100px',
+      display:'block'
+    };
+
+    return (
         <div id="loginbox">
             <form id="loginform" className="form-vertical">
 				        <div className="control-group normal_text"> <h2 id="login_header"><img height="50" width="50" src="/img/authenticase.jpg" alt="Logo" /> Authenticase</h2></div>
@@ -22,22 +30,12 @@ class Login extends Component {
                         </div>
                     </div>
                 </div>
-                <div className="form-actions">
-                    <span className="pull-left"><a className="flip-link btn btn-info" id="to-recover">Lost password?</a></span>
-                    <span className="pull-right"> <a type="submit"  className="btn btn-success" > Login</a></span>
-                </div>
-            </form>
-            <form id="recoverform" action="#" className="form-vertical">
-				        <p className="normal_text">Enter your e-mail address below and we will send you instructions how to recover a password.</p>
-                     <div className="controls">
-                        <div className="main_input_box">
-                            <span className="add-on bg_lo"><i className="icon-envelope"></i></span><input type="text" placeholder="E-mail address" />
-                        </div>
-                    </div>
+
+                <Link to="/dashboard"><button type="button"className="btn btn-success" style={loginBtnStyle}> Log in</button></Link>
 
                 <div className="form-actions">
-                    <span className="pull-left"><a href="/" className="flip-link btn btn-success" id="to-login">&laquo; Back to login</a></span>
-                    <span className="pull-right"><a className="btn btn-info">Reecover</a></span>
+                    <span className="pull-left"><a className="flip-link btn btn-info" id="to-recover">Fogot password?</a></span>
+                    <span className="pull-right"> <Link to="/signup"><a type="submit"  className="btn btn-info"> or Sign up</a></Link></span>
                 </div>
             </form>
         </div>
